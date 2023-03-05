6c9d7eb97d0aeb1111f7908cbba0cabf7b5f3590
DenisseDiaz <96696742+DenisseDiaz@users.noreply.github.com>
2023-03-05 12:44:30 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzWPFWAYgYR0H44m3nExeJ1z_iGthkiJM",
+    authDomain: "spectagram-a1277.firebaseapp.com",
+    projectId: "spectagram-a1277",
+    storageBucket: "spectagram-a1277.appspot.com",
+    messagingSenderId: "544890405672",
+    appId: "1:544890405672:web:6fe30dcc67a2dbd4cc061d"
+  };
